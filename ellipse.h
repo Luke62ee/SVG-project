79055d6afa6f58c26f39ecbde77da17d2bfcf075
Luke Ku <haohsiangku@LukedeMacBook-Air.local>
2024-01-15 14:32:12 -0800

    Added ellipse.h

---
+++ ellipse.h
@@ -0,0 +1,34 @@
+/**
+ * SVG Circle
+ **/
+
+#ifndef ELLIPSE_H
+#define ELLIPSE_H
+
+#include "svg.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Ellipse : public SVG {
+
+public:
+  // operator<< equivalent to write the object out
+  ostream &write(ostream &out) const override;
+
+  // operator>> equivalent to read the object contents
+  istream &read(istream &input) override;
+
+private:
+  // coordinates for center of circle
+  int cx{0}, cy{0};
+
+  // radius of circle
+  int rx{0}, ry{0};
+
+  // color of circle
+  string color{"black"};
+};
+
+#endif
